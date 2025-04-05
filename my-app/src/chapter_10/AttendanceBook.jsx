ba4322658fb51c2bcaee5a4b8e49b3dda2b4b37c
wys0530 <166965895+wys0530@users.noreply.github.com>
2025-04-05 23:38:39 +0900

    feat: AttendanceBook.jsx 파일 추가(10강 실습)

---
+++ my-app/src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const students = [
+  {
+    id: 1,
+    name: "Inje",
+  },
+  {
+    id: 2,
+    name: "Steve",
+  },
+  {
+    id: 3,
+    name: "Bill",
+  },
+  {
+    id: 4,
+    name: "Jeff",
+  },
+];
+
+//student라는 배열로부터 학생 정보가 담긴 객체를 받아
+// 학생들의 이름을 목록 형태로 출력하는 컴포넌트
+//배열을 렌더링 하기 위해 map 함수를 사용함
+//key가 꼭! 필요함!!
+/*
+function AttendanceBook(props) {
+  return (
+    <ul>
+      {students.map((student) => {
+        return <li key={student.id}>{student.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+*/
+
+//포맷팅 된 문자열을 key로 사용할 수 있음
+function AttendanceBook(props) {
+  return (
+    <ul>
+      {students.map((student) => {
+        return <li key={`student-id-${student.id}`}>{student.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+//index 사용하여 만들 수 있음
+/*
+function AttendanceBook(props) {
+    return (
+        <ul>
+            {students.map((student, index) => {
+                return <li key={student.id}>{student.name}</li>;
+            })}
+        </ul>
+    );
+}
+ */
+
+export default AttendanceBook;
